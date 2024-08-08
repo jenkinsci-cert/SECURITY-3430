9efdc47a7d82396553dddba50bb7769d575aa466
Daniel Beck <daniel-beck@users.noreply.github.com>
2024-08-08 13:53:50 +0200

    Initial source code commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# SECURITY-3430 Workaround
+
+This is a Java agent for use with Jenkins controllers that do not have the fix for [SECURITY-3430](https://www.jenkins.io/security/advisory/2024-08-07/#SECURITY-3430) (i.e., up to and including 2.470 and LTS 2.452.3).
+
+It transforms the definition of the class containing the SECURITY-3430 vulnerability to prevent exploitation.
+If that fails for some reason, Jenkins is forcibly stopped.
+
+## Usage
+
+### Use as Java agent (standard use case)
+
+Protect the Jenkins controller process from exploitation:
+
+```bash
+java -javaagent:/path/to/security3430-workaround.jar -jar jenkins.war
+```
+
+### Standalone use
+
+Apply the transformation to the specified `RemoteClassLoader$ClassLoaderProxy.class` class file and write the result to a different file:
+
+```bash
+java -jar /path/to/security3430-workaround.jar <source file> <target file>
+```
+
+This could be used to create minimally modified `remoting.jar` files.
+
+## Configuration
+
+Two Java system properties can be set to change the behavior of this Java agent:
+
+* `io.jenkins.security.Security3430Workaround.DISABLE`:
+  Set this to `true` to disable the class transformation.
+* `io.jenkins.security.Security3430Workaround.SKIP_SHUTDOWN`:
+  Set this to `true` to not stop the Jenkins process when class transformation fails.
+  Only recommended in specific narrow situations, e.g., when closely monitoring log messages (see below).
+
+Both need to set before the class is loaded and an attempt to transform is made, ideally as a `-D` command line option.
+
+## Logging
+
+Log messages use the `io.jenkins.security.Security3430Workaround` logger.
+Messages logged on `SEVERE` indicate a failure to transform.
+
+## Testing
+
+Run the following code in the script console of a connected Jenkins agent to confirm the effectiveness of this workaround:
+
+```groovy
+def cl = Thread.currentThread().getContextClassLoader().proxy
+cl.fetchJar(new URL('file:/path/to/jenkins/home/secrets/master.key'))
+```
+
+With neither fix nor workaround, this will print a message like the following, indicating successful exploitation: `Result: [B@23edd8a0`
+
+With this workaround applied, this will throw an `AbstractMethodError` and print a long stack trace.
+
+## License
+
+Licensed under the terms of the MIT License. Copyright 2024 CloudBees, Inc.
---
+++ src/main/java/io/jenkins/security/Security3430Workaround.java
@@ -0,0 +1,119 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2024, CloudBees, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package io.jenkins.security;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.lang.instrument.Instrumentation;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.security.ProtectionDomain;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Security3430Workaround implements ClassFileTransformer {
+    private static final Logger LOGGER = Logger.getLogger(Security3430Workaround.class.getName());
+
+    @Override
+    public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
+        if (!className.equals("hudson/remoting/RemoteClassLoader$ClassLoaderProxy")) {
+            LOGGER.log(Level.FINEST, () -> "SECURITY-3430 Workaround: Skipping transformation because class name does not match: " + className);
+            return null;
+        }
+
+        final String systemPropertyName = Security3430Workaround.class.getName() + ".DISABLE";
+        if (Boolean.getBoolean(systemPropertyName)) {
+            LOGGER.log(Level.INFO, () -> "SECURITY-3430 Workaround: Skipping transformation of " + className + " because " + systemPropertyName + " is set");
+            return null;
+        }
+
+        LOGGER.log(Level.INFO, () -> "SECURITY-3430 Workaround: Performing transformation of " + className);
+
+        final byte[] transformed = innerTransform(classfileBuffer);
+        if (transformed != null) {
+            return transformed;
+        }
+
+        LOGGER.log(Level.SEVERE, () -> "SECURITY-3430 Workaround: Failed to find the 'fetchJar' in the class file, cannot prevent exploitation.");
+        final String skipShutdownPropertyName = Security3430Workaround.class.getName() + ".SKIP_SHUTDOWN";
+        if (Boolean.getBoolean(skipShutdownPropertyName)) {
+            LOGGER.log(Level.SEVERE, () -> "SECURITY-3430 Workaround: Skipping shutdown because " + skipShutdownPropertyName + " is set. The instance is not protected from SECURITY-3430.");
+        } else {
+            LOGGER.log(Level.SEVERE, () -> "SECURITY-3430 Workaround: Shutting down.");
+            System.exit(1);
+        }
+        return null;
+    }
+
+    private static byte[] innerTransform(byte[] classfileBuffer) {
+        byte[] needle = "fetchJar".getBytes(StandardCharsets.US_ASCII);
+
+        OUTER: for (int i = 0; i < classfileBuffer.length; i++) {
+            for (int j = 0; j < needle.length; j++) {
+                if (classfileBuffer[i+j] != needle[j]) {
+                    continue OUTER;
+                }
+                if (j == needle.length - 1) {
+                    return innerReplace(classfileBuffer, i + j);
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private static byte[] innerReplace(byte[] classfileBuffer, int offset) {
+        final byte[] output = Arrays.copyOf(classfileBuffer, classfileBuffer.length);
+        output[offset] = 'm'; // fetchJar -> fetchJam :-)
+        return output;
+    }
+
+    public static void premain(String args, Instrumentation instrumentation) {
+        LOGGER.log(Level.INFO, () -> "Setting up " + Security3430Workaround.class.getName());
+        instrumentation.addTransformer(new Security3430Workaround());
+    }
+
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
+            System.err.println("This file is a Java agent addressing SECURITY-3430/CVE-2024-43044 in older releases of Jenkins by patching bytecode.");
+            System.err.println("Usage:");
+            System.err.println("    java -javaagent:/path/to/security3430-workaround.jar -jar jenkins.war");
+            System.err.println("Additionally, this file can be used as an executable jar to patch a RemoteClassLoader$ClassLoaderProxy.class file.");
+            System.err.println("Usage:");
+            System.err.println("    java -jar /path/to/security3430-workaround.jar <source> <target>");
+            return;
+        }
+        final byte[] original = Files.readAllBytes(new File(args[0]).toPath());
+        final byte[] modified = innerTransform(original);
+        if (modified == null) {
+            System.err.println("Failed to transform the specified file. Is it a RemoteClassLoader$ClassLoaderProxy.class?");
+            return;
+        }
+        Files.write(new File(args[1]).toPath(), modified);
+    }
+}
