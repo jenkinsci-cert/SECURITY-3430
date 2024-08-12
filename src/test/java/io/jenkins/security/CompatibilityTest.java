16511a6a31e52ff58f90d1594938c84dc4487e4c
Daniel Beck <daniel-beck@users.noreply.github.com>
2024-08-12 18:26:19 +0200

    Use a reference file to find the directory with all the remotings

--- src/test/java/io/jenkins/security/CompatibilityTest.java
+++ src/test/java/io/jenkins/security/CompatibilityTest.java
@@ -4,13 +4,12 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
+import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.Objects;
 import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
-import java.util.zip.ZipInputStream;
 import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -18,23 +17,25 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
 public class CompatibilityTest {
 
     private static final String REMOTING_JAR_NAME_PATTERN = "remoting[-](.+)[.]jar";
-    private static final String REMOTING_JAR_DIRECTORY = "target/classes/old-remoting/";
 
-    private static Stream<Arguments> test() {
-        return Arrays.stream(Objects.requireNonNull(new File(REMOTING_JAR_DIRECTORY).list((dir, name) -> name.matches(REMOTING_JAR_NAME_PATTERN)))).map(Arguments::of);
+    private static Stream<Arguments> test() throws URISyntaxException {
+        return Arrays.stream(Objects.requireNonNull(getRemotingDirectory().list((dir, name) -> name.matches(REMOTING_JAR_NAME_PATTERN)))).map(Arguments::of);
     }
     @ParameterizedTest
     @MethodSource
-    public void test(String remotingFileName) throws IOException {
-        final File remotingFile = new File(REMOTING_JAR_DIRECTORY + remotingFileName);
+    public void test(String remotingFileName) throws IOException, URISyntaxException {
+        final File remotingFile = new File(getRemotingDirectory(), remotingFileName);
         assertTrue(remotingFile.exists(), "Failed to find remoting " + remotingFileName);
         final byte[] classfile = readClassfileFromRemoting(new ZipFile(remotingFile));
         assertNotNull(Security3430Workaround.innerTransform(classfile), "Failed to transform class file in remoting " + remotingFileName);
+    }
+
+    private static File getRemotingDirectory() throws URISyntaxException {
+        return new File(Objects.requireNonNull(CompatibilityTest.class.getResource("/old-remoting/remoting-4.0.jar")).toURI().getPath()).getParentFile();
     }
 
     private static byte[] readClassfileFromRemoting(ZipFile zipFile) throws IOException {
